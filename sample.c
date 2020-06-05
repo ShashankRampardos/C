7df6953481c9c44f436cfe2272ff716704ab19d1
Shashank Vashistha <66352865+shashankrampardo@users.noreply.github.com>
2020-06-05 08:17:30 +0530

    Updated

--- sample.c
+++ sample.c
@@ -3,15 +3,15 @@
 //Program to find third angle of a triangle and re use the program in run time without terminating it.
 main()
 {
-   int a,b;
-   char sel;
+   int a,b;//to enter two angles
+   char sel;//to enter y or n.
 start://goto tag
    printf("\nEnter two angles:");
    scanf("%d%d",&a,&b);
    printf("\nThird angle is %d",180-(a+b));
 middle://goto tag
    printf("\nDO you want to continue(yes/no):");
-   scanf("%c",&sel);//ISSUE 1:scanf() not run in first time and control branch to else part at the end,why? Please run the program to understand what I'm saying.
+   scanf("%c",&sel);//ISSUE 1:scanf() not run in first time and control branch to ELSE without taking input to "sel" variable, why? Please run the program to understand what I'm saying.
    fflush(stdin);//user input yes or no which is a string but we check only first letter then remaining letters stay left in STDIN so fflush() used here.
    if(sel=='y'||sel=='Y')//in case user enter yes or Yes we only check first letter
     {   getch();
